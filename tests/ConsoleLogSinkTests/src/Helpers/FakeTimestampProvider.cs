0d38e50f16559f2d59c6930ab8230fafcc383878
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-03-02 22:11:07 +0100

    change: moved ILogger to WB.Logging.Base change: moved ILogSink to WB.Logging.Base change: moved LogLevel to WB.Logging.Base change: moved LogMessage to WB.Logging.Base change: moved ITimestampProvider to WB.Logging.Base change: moved ConsoleLogSink to WB.Logging.LogSinks.Console

--- tests/ConsoleLogSinkTests/src/Helpers/FakeTimestampProvider.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using WB.Logging;
-
-namespace ConsoleLogSinkTests;
-
-internal sealed class FakeTimestampProvider : ITimestampProvider
-{
-    public DateTimeOffset CurrentTimestamp => DateTimeOffset.MinValue;
-}
